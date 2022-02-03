57c2a502e48a296637725fc75d4bd9d4e1968ec8
NafiAsib <nafi.asib@gmail.com>
2022-02-03 17:03:13 +0600

    buttons added in navbar

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -4,15 +4,40 @@ const Navbar = () => {
     return (
         <>
             <nav className="navbar navbar-light bg-light">
-                <span className="navbar-brand" style={{ marginLeft: '40px' }}>
-                    <img
-                        src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
-                        width="60"
-                        height="50"
-                        className="d-inline-block align-top"
-                        alt="imdb-logo"
-                        style={{ objectFit: 'contain' }}
-                    />
+                <span className="navbar-brand d-flex align-items-center">
+                    <span style={{ marginLeft: '40px', marginRight: '40px' }}>
+                        <img
+                            src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
+                            width="60"
+                            height="50"
+                            className="d-inline-block align-top"
+                            alt="imdb-logo"
+                            style={{ objectFit: 'contain' }}
+                        />
+                    </span>
+                    <button
+                        className="btn btn-warning"
+                        style={{ marginRight: '10px' }}
+                    >
+                        All movies
+                    </button>
+                    <button className="btn btn-outline-warning">
+                        Add movie
+                    </button>
+                </span>
+                <span>
+                    <button
+                        className="btn btn-outline-warning"
+                        style={{ marginRight: '10px' }}
+                    >
+                        Login
+                    </button>
+                    <button
+                        className="btn btn-outline-warning"
+                        style={{ marginRight: '40px' }}
+                    >
+                        Signup
+                    </button>
                 </span>
             </nav>
         </>
