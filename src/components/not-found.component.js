5779356dc0145fc403e84df1c974024cb1896868
NafiAsib <nafi.asib@gmail.com>
2022-02-03 18:25:24 +0600

    added height

--- src/components/not-found.component.js
+++ src/components/not-found.component.js
@@ -2,9 +2,12 @@ import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div className="container d-flex flex-column justify-content-center align-items-center">
+        <div
+            className="container d-flex flex-column justify-content-center align-items-center"
+            style={{ height: '50vh' }}
+        >
             <h1 className="display-1">404</h1>
-            <h1 className="display-4">
+            <h1 className="display-4 text-center">
                 The page you're looking for doesn't exists
             </h1>
             <Link to="/" className="btn">
