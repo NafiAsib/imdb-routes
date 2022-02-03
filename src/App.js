ef748ac57d9106275bbdbf97fd8e951ade7520eb
NafiAsib <nafi.asib@gmail.com>
2022-02-03 17:13:14 +0600

    added routes with path & component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-import Movies from './components/movies.component';
 import Navbar from './components/navbar.component';
+import Home from './components/home.component';
+import Movies from './components/movies.component';
+import AddMovie from './components/add-movie.component';
+import Login from './components/login.component';
+import Signup from './components/signup.component';
+import NotFound from './components/not-found.component';
 
 const App = () => {
     return (
         <>
             <Navbar />
-            <Movies />
+            <Switch>
+                <Route path="/movies" component={Movies} />
+                <Route path="/add-movie" component={AddMovie} />
+                <Route path="/login" component={Login} />
+                <Route path="/signup" component={Signup} />
+                <Route exact path="/" component={Home} />
+                <Route component={NotFound} />
+            </Switch>
         </>
     );
 };
