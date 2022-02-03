386e2326fa9be2c950a899132e32a3de9b0267b2
NafiAsib <nafi.asib@gmail.com>
2022-02-03 17:29:29 +0600

    added links to buttons

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <>
             <nav className="navbar navbar-light bg-light">
                 <span className="navbar-brand d-flex align-items-center">
-                    <span style={{ marginLeft: '40px', marginRight: '40px' }}>
+                    <Link
+                        to="/"
+                        style={{ marginLeft: '40px', marginRight: '40px' }}
+                    >
                         <img
                             src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
                             width="60"
@@ -14,30 +18,33 @@ const Navbar = () => {
                             alt="imdb-logo"
                             style={{ objectFit: 'contain' }}
                         />
-                    </span>
-                    <button
+                    </Link>
+                    <Link
+                        to="/movies"
                         className="btn btn-warning"
                         style={{ marginRight: '10px' }}
                     >
                         All movies
-                    </button>
-                    <button className="btn btn-outline-warning">
+                    </Link>
+                    <Link to="/add-movie" className="btn btn-outline-warning">
                         Add movie
-                    </button>
+                    </Link>
                 </span>
                 <span>
-                    <button
+                    <Link
+                        to="/login"
                         className="btn btn-outline-warning"
                         style={{ marginRight: '10px' }}
                     >
                         Login
-                    </button>
-                    <button
+                    </Link>
+                    <Link
+                        to="/signup"
                         className="btn btn-outline-warning"
                         style={{ marginRight: '40px' }}
                     >
                         Signup
-                    </button>
+                    </Link>
                 </span>
             </nav>
         </>
