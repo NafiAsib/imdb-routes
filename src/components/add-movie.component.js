0cd9b33425024f63050bd6c35dfd81cc21af88f8
NafiAsib <nafi.asib@gmail.com>
2022-02-03 18:02:33 +0600

    added add-movie page

---
+++ src/components/add-movie.component.js
@@ -0,0 +1,9 @@
+const AddMovie = () => {
+    return (
+        <div className="container text-center">
+            <h1 className="display-1">Add a new movie</h1>
+        </div>
+    );
+};
+
+export default AddMovie;
