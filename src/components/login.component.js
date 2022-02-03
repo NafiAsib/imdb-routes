95cbdaa3451c004e6b8fb48380d1a71137643cb5
NafiAsib <nafi.asib@gmail.com>
2022-02-03 18:04:42 +0600

    added login and signup components

---
+++ src/components/login.component.js
@@ -0,0 +1,9 @@
+const Login = () => {
+    return (
+        <div className="container text-center">
+            <h1 className="display-1">Login</h1>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/components/signup.component.js
@@ -0,0 +1,9 @@
+const Signup = () => {
+    return (
+        <div className="container text-center">
+            <h1 className="display-1">Signup</h1>
+        </div>
+    );
+};
+
+export default Signup;
